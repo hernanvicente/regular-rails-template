6bab3ce1d2df335aa4b09b9a20b6fc4c637b7693
Hernan Maguina <hernan.maguina@gmail.com>
2020-09-16 00:13:53 +0200

    Update authentication gems and syntax

--- generator.rb
+++ generator.rb
@@ -7,13 +7,25 @@ simple_command = yes? 'Will we use service objects with simple_command?'
 friendly_id = yes? 'Will we use slugs with friendly id?'
 change_template_language = yes? 'Will we use other template language?'
 admin_database = yes? 'Will we use an web admin database?'
-devise = yes? 'Will we authenticate with devise?'
+authentication = yes? 'Will we authenticate?'
 sitemap = yes? 'Will we need a sitemap generator?'
-
+pagination = yes? 'Will we paginate with kaminari?'
 
 # Global Gems
-gem 'kaminari'
-gem 'devise' if devise
+gem 'kaminari' if pagination
+
+if authentication
+  authentication_gem = ask('Which authentication gem would you like to use?',
+                           limited_to: %w[clearance devise sorcery])
+  case authentication_gem
+  when 'clearance'
+    gem 'clearance'
+  when 'devise'
+    gem 'devise'
+  when 'sorcery'
+    gem 'sorcery'
+  end
+end
 
 if api_mode
   gem 'active_model_serializers'
@@ -97,9 +109,13 @@ gsub_file 'config/database.yml', 'application_database', database_name.to_s
 
 # Database credentials
 database_user_name = ask('Enter the database user name. Press <enter> to skip.')
-gsub_file 'config/database.yml', '#user: database_user_name', "user: #{database_user_name}" unless database_user_name.empty?
+unless database_user_name.empty?
+  gsub_file 'config/database.yml', '#user: database_user_name', "user: #{database_user_name}"
+end
 database_user_password = ask('Enter the database user password. Press <enter> to skip.')
-gsub_file 'config/database.yml', '#password: database_user_password', "password: #{database_user_password}" unless database_user_password.empty?
+unless database_user_password.empty?
+  gsub_file 'config/database.yml', '#password: database_user_password', "password: #{database_user_password}"
+end
 
 # Run bundle to install gems
 run 'bundle'
@@ -118,52 +134,48 @@ route "root to: 'pages#home'" if pages_controller
 def add_devise
   generate 'devise:install'
   generate 'devise User'
-
+  rake('db:migrate')
   environment "config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }",
               env: 'development'
 end
 
-def add_administrate
-  generate "administrate:install"
+def add_administrate(authentication, authentication_gem)
+  generate 'administrate:install'
 
-  append_to_file "app/assets/config/manifest.js" do
+  append_to_file 'app/assets/config/manifest.js' do
     "//= link administrate/application.css\n//= link administrate/application.js"
   end
 
-  gsub_file "app/dashboards/announcement_dashboard.rb",
-    /announcement_type: Field::String/,
-    "announcement_type: Field::Select.with_options(collection: Announcement::TYPES)"
+  if authentication && authentication_gem == ('devise' || 'clearance')
+    gsub_file 'app/dashboards/user_dashboard.rb',
+              /email: Field::String/,
+              "email: Field::String,\n    password: Field::String.with_options(searchable: false)"
 
-  gsub_file "app/dashboards/user_dashboard.rb",
-    /email: Field::String/,
-    "email: Field::String,\n    password: Field::String.with_options(searchable: false)"
+    gsub_file 'app/dashboards/user_dashboard.rb',
+              /FORM_ATTRIBUTES = \[/,
+              "FORM_ATTRIBUTES = [\n    :password,"
 
-  gsub_file "app/dashboards/user_dashboard.rb",
-    /FORM_ATTRIBUTES = \[/,
-    "FORM_ATTRIBUTES = [\n    :password,"
+    gsub_file 'app/controllers/admin/application_controller.rb',
+              /# TODO Add authentication logic here\./,
+              "redirect_to '/', alert: 'Not authorized.' unless user_signed_in? && current_user.admin?"
+  end
 
-  gsub_file "app/controllers/admin/application_controller.rb",
-    /# TODO Add authentication logic here\./,
-    "redirect_to '/', alert: 'Not authorized.' unless user_signed_in? && current_user.admin?"
-
-  environment do <<-RUBY
+  environment do
+    <<-RUBY
     # Expose our application's helpers to Administrate
     config.to_prepare do
       Administrate::ApplicationController.helper #{@app_name.camelize}::Application.helpers
     end
-  RUBY
+    RUBY
   end
 end
 
 def add_sitemap
-  rails_command "sitemap:install"
+  rails_command 'sitemap:install'
 end
-
 
 # Config rack cors
-if api_mode
-  run "cp #{path}/templates/config/initializers/cors.rb config/initializers/cors.rb"
-end
+run "cp #{path}/templates/config/initializers/cors.rb config/initializers/cors.rb" if api_mode
 
 # Ask me if we want to run migration
 rake('db:migrate') if yes?('Run db:migrate?')
@@ -178,17 +190,17 @@ insert_into_file 'test/test_helper.rb', after: /class ActiveSupport::TestCase\n/
   "  include FactoryBot::Syntax::Methods\n"
 end
 
-insert_into_file 'test/test_helper.rb', after: /require \'rails\/test_help\'\n/ do
+insert_into_file 'test/test_helper.rb', after: %r{require \'rails/test_help\'\n} do
   "  Shoulda::Matchers.configure do |config|\n" \
   "    config.integrate do |with|\n" \
   "      with.test_framework :minitest\n" \
   "      with.library :rails\n" \
   "    end\n" \
-  "  end"
+  '  end'
 end
 
 # Minitest Reporters
-insert_into_file 'test/test_helper.rb', after: /require \'rails\/test_help\'\n/ do
+insert_into_file 'test/test_helper.rb', after: %r{require \'rails/test_help\'\n} do
   "  require 'minitest/reporters'\n" \
   "  Minitest::Reporters.use!\n" \
   "\n"
@@ -199,10 +211,12 @@ insert_into_file 'config/environments/development.rb', before: /^end/ do
   "  # From https://github.com/johnbintz/rack-livereload\n  config.middleware.use Rack::LiveReload\n"
 end
 
+# Run devise
+add_devise if authentication_gem == 'devise'
+
 # Run admin installer
 if admin_database
-  add_administrate if web_admin_database == 'administrate'
-  add_devise       if web_admin_database == 'rails_admin'
+  add_administrate(authentication, authentication_gem) if web_admin_database == 'administrate'
 end
 
 # Sitemap
